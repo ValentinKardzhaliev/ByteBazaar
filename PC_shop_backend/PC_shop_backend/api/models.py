5a1cccf57472ac689a25910fe01b0b40e064ecbf
ValentinKardzhaliev <vkardjaliev@abv.bg>
2023-12-09 22:12:18 +0200

    Made the phone field not unique

--- PC_shop_backend/PC_shop_backend/api/models.py
+++ PC_shop_backend/PC_shop_backend/api/models.py
@@ -6,7 +6,7 @@ from phonenumber_field.modelfields import PhoneNumberField
 class ByteBazaarUserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     email = models.EmailField(unique=True)
-    phone = PhoneNumberField(unique=True, null=True, blank=True, default="")
+    phone = PhoneNumberField(null=True, blank=True, default="")
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
