806574b218769c50cbe344f3cb36819a80d3a043
ValentinKardzhaliev <vkardjaliev@abv.bg>
2023-12-13 22:06:08 +0200

    Removed the price from displaying for now. Adjusted the button positioning

--- pc_shop_frontend/src/components/ProductList/Product/Product.jsx
+++ pc_shop_frontend/src/components/ProductList/Product/Product.jsx
@@ -9,7 +9,6 @@ function Product(props) {
             <img src={`http://localhost:8000${props.product.image}`} alt={props.product.name} className="product-image" />
             <div className="product-details">
                 <h2 className="product-name">{props.product.name}</h2>
-                <h2>${props.product.price}</h2>
                 <span className="product-description"><p> - {props.product.description}</p></span>
             </div>
             <Link to={`/details/${props.product.id}`} className="details-link">Details</Link>
