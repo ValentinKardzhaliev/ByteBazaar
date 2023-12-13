69ff53a69e067af7d5a16f59f4a242388223465f
ValentinKardzhaliev <vkardjaliev@abv.bg>
2023-12-13 21:50:30 +0200

    fixed the product description overflowing and impacting the button negatively in the home page

--- pc_shop_frontend/src/components/ProductList/Product/Product.jsx
+++ pc_shop_frontend/src/components/ProductList/Product/Product.jsx
@@ -7,10 +7,11 @@ function Product(props) {
     return (
         <li key={props.product.id} className="product-item">
             <img src={`http://localhost:8000${props.product.image}`} alt={props.product.name} className="product-image" />
-            <span className="product-details">
+            <div className="product-details">
                 <h2 className="product-name">{props.product.name}</h2>
+                <h2>${props.product.price}</h2>
                 <span className="product-description"><p> - {props.product.description}</p></span>
-            </span>
+            </div>
             <Link to={`/details/${props.product.id}`} className="details-link">Details</Link>
         </li>
     )
