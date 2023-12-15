c3b3ae9e7684d1d465a55e4c03550690e31020ee
Bogomil-Simitchiev <simitchiev365@abv.bg>
2023-12-15 18:16:08 +0200

    Removed description and replaced it with price to show up in the SearchProduct component too

--- pc_shop_frontend/src/components/SearchProductList/SearchProduct/SearchProduct.jsx
+++ pc_shop_frontend/src/components/SearchProductList/SearchProduct/SearchProduct.jsx
@@ -7,9 +7,8 @@ function SearchProduct(props) {
             <img src={`http://localhost:8000${props.product.image}`} alt={props.product.name} className="product-image" />
             <div className="product-details">
                 <h2 className="product-name">{props.product.name}</h2>
-                <span className="product-description"><p> - {props.product.description}</p></span>
-                <h2 className="product-name">${props.product.price}</h2>
             </div>
+            <span className="product-price"><p>$ {props.product.price}</p></span>
             <Link to={`/details/${props.product.id}`} className="details-link">Details</Link>
         </li>
     )
