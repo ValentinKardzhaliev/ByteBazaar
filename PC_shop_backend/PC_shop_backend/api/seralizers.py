84ae3dfec5e5a5f92f4dc708a02b9c010b045ac1
Bogomil-Simitchiev <simitchiev365@abv.bg>
2023-12-15 17:44:56 +0200

    Changed phone field to be optional and auth urls

--- PC_shop_backend/PC_shop_backend/api/seralizers.py
+++ PC_shop_backend/PC_shop_backend/api/seralizers.py
@@ -15,7 +15,7 @@ class UserSerializer(serializers.ModelSerializer):
 
 class CreateUserSerializer(serializers.ModelSerializer):
     password_confirmation = serializers.CharField(write_only=True)
-    phone = serializers.CharField(write_only=True, required=False)
+    phone = serializers.CharField(write_only=True, required=False, allow_blank=True)
 
     class Meta:
         model = UserModel
