e673895039e4fbf940bba2a586710a72c2fcca7a
ValentinKardzhaliev <vkardjaliev@abv.bg>
2023-12-19 13:36:21 +0200

    Made all info of the products be given to the api in the index page

--- PC_shop_backend/PC_shop_backend/common/serializers.py
+++ PC_shop_backend/PC_shop_backend/common/serializers.py
@@ -2,13 +2,43 @@ from django.db.models import Q
 from rest_framework import serializers
 
 from .models import Product
+from ..catalog.models import Computer, Monitor, Keyboard
+from ..catalog.serializers import ComputerSerializer, MonitorSerializer, KeyboardSerializer
+
 
 class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    name = serializers.CharField(max_length=50)
-    image = serializers.ImageField()
-    price = serializers.DecimalField(max_digits=10, decimal_places=2)
-    description = serializers.CharField()
+    def __init__(self, *args, **kwargs):
+        super(ProductSerializer, self).__init__(*args, **kwargs)
+
+        # Get the fields from the abstract model 'Product'
+        product_fields = [field.name for field in Product._meta.get_fields()]
+
+        # Add fields from the abstract model to the serializer
+        for field_name in product_fields:
+            self.fields[field_name] = serializers.CharField()
+
+        # Get all concrete subclasses of 'Product' dynamically
+        concrete_models = Product.__subclasses__()
+
+        # Add fields from each concrete model to the serializer dynamically
+        for model in concrete_models:
+            concrete_fields = [field.name for field in model._meta.get_fields() if field.name not in product_fields]
+            for field_name in concrete_fields:
+                self.fields[field_name] = serializers.CharField(required=False)
+
+    def to_representation(self, instance):
+        # Use specific serializers for concrete models
+        if isinstance(instance, Computer):
+            serializer = ComputerSerializer(instance)
+        elif isinstance(instance, Monitor):
+            serializer = MonitorSerializer(instance)
+        elif isinstance(instance, Keyboard):
+            serializer = KeyboardSerializer(instance)
+        else:
+            serializer = super(ProductSerializer, self)
+
+        return serializer.data
+
 
 class ProductSearchSerializer(serializers.Serializer):
     search_query = serializers.CharField(
