fe777fe15e25d12ad5828ffaa10c3073a010b8e3
Bogomil-Simitchiev <simitchiev365@abv.bg>
2024-01-08 21:07:37 +0200

    Created ProductDetails page and added characteristics to SearchProduct component

--- pc_shop_frontend/src/components/SearchProductList/SearchProduct/SearchProduct.jsx
+++ pc_shop_frontend/src/components/SearchProductList/SearchProduct/SearchProduct.jsx
@@ -1,6 +1,45 @@
 import { Link } from 'react-router-dom';
 
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 function SearchProduct(props) {
+    let characteristics = {
+        computer: [],
+        monitor: [],
+        keyboard: []
+    };
+    let typeOfProduct = props.product.type;
+
+    switch (typeOfProduct) {
+        case 'computer':
+            characteristics[typeOfProduct].push(
+                props.product.processor,
+                props.product.graphics,
+                props.product.memory,
+                props.product.storage
+            );
+
+            break;
+        case 'monitor':
+            characteristics[typeOfProduct].push(
+                props.product.resolution,
+                props.product.size
+            );
+
+            break;
+        case 'keyboard':
+            characteristics[typeOfProduct].push(
+                capitalizeFirstLetter(props.product.key_switch_type),
+                props.product.backlight ? 'Backlit' : 'Not Backlit', // Display a message based on the boolean value
+                props.product.color
+            );
+            break;
+
+        default:
+            break;
+    }
 
     return (
         <li className="product-item">
@@ -8,6 +47,10 @@ function SearchProduct(props) {
             <div className="product-details">
                 <h2 className="product-name">{props.product.name}</h2>
             </div>
+
+            <ul className='characteristics'>
+                {characteristics[typeOfProduct].map((c, index) => <li key={index}>{c}</li>)}
+            </ul>
             <span className="product-price"><p>$ {props.product.price}</p></span>
             <Link to={`/products/${props.product.type}/${props.product._id}`} className="details-link">Details</Link>
         </li>
