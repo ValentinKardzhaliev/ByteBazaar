0b113ab06e33dd84e3c0d91e786c9be5e6f71f8e
Bogomil-Simitchiev <simitchiev365@abv.bg>
2024-01-24 21:46:42 +0200

    Created PriceFilter component and started implementing GraphicFilter component

--- pc_shop_frontend/src/components/ComputerList/ComputerList.jsx
+++ pc_shop_frontend/src/components/ComputerList/ComputerList.jsx
@@ -2,6 +2,8 @@ import { useContext, useEffect, useState } from 'react';
 import './ComputerList.css'
 import Computer from './Computer/Computer';
 import LoadingContext from '../../contexts/LoadingContext';
+import PriceFilter from '../Filters/PriceFilter';
+import Checkbox from '../Filters/ComputerFilters/GraphicFilter';
 
 function ComputerList() {
     const [computers, setComputers] = useState([]);
@@ -13,14 +15,19 @@ function ComputerList() {
             .then(comps => {
                 setComputers(comps);
                 stopLoading();
-            });
+            }).catch(err => console.log(err));
     }, []);
 
     return (
-        <ul className="computer-list">
-            {isLoading ? <h1>Loading...</h1> : computers.length === 0 && <h1>No computers found!</h1>}
-            {computers.map(computer => <Computer key={computer._id} product={computer} />)}
-        </ul>
+        <>
+            <PriceFilter setComputers={setComputers} />
+            <Checkbox graphics={'Nvidia'} setComputers={setComputers} />
+            <Checkbox graphics={'AMD'} setComputers={setComputers} />
+            <ul className="computer-list">
+                {isLoading ? <h1>Loading...</h1> : computers.length === 0 && <h1>No computers found!</h1>}
+                {computers.map(computer => <Computer key={computer._id} product={computer} />)}
+            </ul>
+        </>
     )
 }
 
