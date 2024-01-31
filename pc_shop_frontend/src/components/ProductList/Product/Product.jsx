4e4e528f96d3836531dea07b8eb310185000a983
ValentinKardzhaliev <vkardjaliev@abv.bg>
2024-01-31 17:36:15 +0200

    Fixed html stucture inside Product.jsx and fixed product name being hidden by charactaristics. Adjusted font-sizing for better responsiveness

--- pc_shop_frontend/src/components/ProductList/Product/Product.jsx
+++ pc_shop_frontend/src/components/ProductList/Product/Product.jsx
@@ -15,10 +15,10 @@ function Product(props) {
             {firstImage}
             <div className="product-details">
                 <h2 className="product-name">{props.product.name}</h2>
+                <ul className='characteristics'>
+                    {characteristics[typeOfProduct].map((c, index) => <li key={index}>{c}</li>)}
+                </ul>
             </div>
-            <ul className='characteristics'>
-                {characteristics[typeOfProduct].map((c, index) => <li key={index}>{c}</li>)}
-            </ul>
             <span className="product-price"><p>$ {props.product.price}</p></span>
             <Link to={`/products/${props.product.type}/${props.product._id}`} className="details-link">Details</Link>
         </li>
