a908c8fc4d3f3b71b19d8a5b62b72d8dd98e061e
ValentinKardzhaliev <vkardjaliev@abv.bg>
2024-02-29 00:15:46 +0200

    Added the prototype of the design of the user liked products page

--- pc_shop_frontend/src/components/UserLikedProducts/UserLikedProducts.jsx
+++ pc_shop_frontend/src/components/UserLikedProducts/UserLikedProducts.jsx
@@ -3,6 +3,10 @@ import { getAllLikedForUser } from "../../services/productService";
 import { Link } from "react-router-dom";
 import "./UserLikedProducts.css"; // Define your CSS styles for responsiveness here
 import AuthContext from "../../contexts/AuthContext";
+import { faShoppingCart, faHeart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+
 
 const UserLikedProducts = () => {
     const { user } = useContext(AuthContext);
@@ -24,24 +28,40 @@ const UserLikedProducts = () => {
 
     return (
         <div className="user-liked-products-container">
-            <h2>Liked Products</h2>
-            {loading ? (
-                <p>Loading...</p>
-            ) : (
-                <div className="user-liked-products">
-                    {likedProducts.map((product) => (
-                        <div className="product-card" key={product._id}>
-                            <Link to={`/product/${product._id}`}>
-                                <img src={`http://localhost:8000${product.images[0].image}`} alt={product.name} className="productliked-image" />
-                            </Link>
-                            <div className="productliked-details">
-                                <Link to={`/product/${product._id}`} className="productliked-name">{product.name}</Link>
-                                <button className="add-to-cart-button">Add to Cart</button>
+            <div className="user-liked-products-content-wrapper">
+                <h2>Liked Products</h2>
+                {loading ? (
+                    <p>Loading...</p>
+                ) : (
+                    <div className="user-liked-products">
+                        {likedProducts.map((product) => (
+                            <div className="product-card" key={product._id}>
+                                <Link to={`/product/${product._id}`}>
+                                    <img src={`http://localhost:8000${product.images[0].image}`} alt={product.name} className="productliked-image" />
+                                </Link>
+                                <div className="productliked-details">
+                                    <h2 className="productliked-name">{product.name}</h2>
+                                    <div className="details-inner-container">
+                                        <Link to={`/product/${product._id}`} className="product-detailsPage-link">View Full Characteristics</Link>
+                                        <div className="product-price-container">
+                                            <span className="label">Price:</span>
+                                            <span className="productliked-price">{product.price}$</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="productliked-actions">
+                                    <a href="#" className="productliked-unlike" role="button">
+                                        <FontAwesomeIcon icon={faHeart} />
+                                    </a>
+                                    <button className="productliked-add-to-cart" onClick={() => handleAddToCart(product)}>
+                                        Add to Cart <FontAwesomeIcon icon={faShoppingCart} />
+                                    </button>
+                                </div>
                             </div>
-                        </div>
-                    ))}
-                </div>
-            )}
+                        ))}
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
