0f6718c2509ab9aea32223cd650602dd45e60031
ValentinKardzhaliev <vkardjaliev@abv.bg>
2024-03-01 16:07:49 +0200

    Added a view for getting user's cart

--- PC_shop_backend/PC_shop_backend/cart/urls.py
+++ PC_shop_backend/PC_shop_backend/cart/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from PC_shop_backend.cart.views import add_to_cart
+from PC_shop_backend.cart.views import add_to_cart, get_user_cart
 
 urlpatterns = [
     path('add/<uuid:product_id>/', add_to_cart, name='add_to_cart'),
+    path('user_cart/', get_user_cart, name='user_cart'),
 ]
--- PC_shop_backend/PC_shop_backend/cart/views.py
+++ PC_shop_backend/PC_shop_backend/cart/views.py
@@ -12,27 +12,22 @@ from PC_shop_backend.common.serializers import ProductSerializer
 @api_view(['POST'])
 def add_to_cart(request, product_id):
     try:
-        # Assuming product_id is a valid UUID string
         product = None
 
-        # Iterate through concrete models to find the correct one
         for model_class in Product.__subclasses__():
             try:
                 product = model_class.objects.get(pk=product_id)
-                break  # Exit the loop if a valid product instance is found
+                break
             except model_class.DoesNotExist:
                 continue
 
         if not product:
             return Response({'error': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
 
-        # Determine the product type dynamically
         product_type = product.__class__.__name__.lower()
 
-        # Get or create a cart for the user
         user_cart, created = Cart.objects.get_or_create(user=request.user)
 
-        # Create or update CartItem
         cart_item, created = CartItem.objects.get_or_create(
             user=request.user,
             product_type=product_type,
@@ -44,13 +39,10 @@ def add_to_cart(request, product_id):
             cart_item.quantity += 1
             cart_item.save()
 
-        # Update the user's cart
         user_cart.items.add(cart_item)
 
-        # Serialize the product instance
         product_serializer = ProductSerializer(product)
 
-        # Serialize the cart
         cart_serializer = CartSerializer(user_cart)
 
         response_data = {
@@ -62,3 +54,14 @@ def add_to_cart(request, product_id):
 
     except ValueError:
         return Response({'error': 'Invalid UUID format for product_id'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def get_user_cart(request):
+    # Retrieve the current user's cart
+    user_cart, created = Cart.objects.get_or_create(user=request.user)
+
+    # Serialize the cart data
+    cart_serializer = CartSerializer(user_cart)
+
+    return Response(cart_serializer.data, status=status.HTTP_200_OK)
