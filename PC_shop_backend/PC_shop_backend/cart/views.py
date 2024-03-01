a3bb03247e32027fd13a1b54a493e75b54708cc2
ValentinKardzhaliev <vkardjaliev@abv.bg>
2024-03-01 18:08:06 +0200

    Added additional message to the drecrease quantity view

--- PC_shop_backend/PC_shop_backend/cart/views.py
+++ PC_shop_backend/PC_shop_backend/cart/views.py
@@ -135,3 +135,5 @@ def decrease_quantity(request, product_id):
         cart_item.quantity -= 1
         cart_item.save()
         return JsonResponse({'message': 'Quantity decreased successfully'})
+    else:
+        return JsonResponse({'message': 'Quantity cannot be decreased below 1'})
