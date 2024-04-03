f23e771f894e299ce2241dbe5c2a6c8742f3a70c
ValentinKardzhaliev <vkardjaliev@abv.bg>
2024-04-03 17:51:28 +0300

    Registered the Cart model in Django Admin

--- PC_shop_backend/PC_shop_backend/common/admin.py
+++ PC_shop_backend/PC_shop_backend/common/admin.py
@@ -1,1 +1,11 @@
 from django.contrib import admin
+
+from PC_shop_backend.cart.models import Cart
+
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['user', 'token']
+    list_filter = ['user']
+    search_fields = ['user__username']
+
+admin.site.register(Cart, CartAdmin)
