59a313604c6fbf18c04c92798370cc3db77aec01
Bogomil-Simitchiev <simitchiev365@abv.bg>
2024-04-06 01:12:22 +0300

    Created periphery menu and deleted package-lock.js

--- pc_shop_frontend/src/components/Header/Header.jsx
+++ pc_shop_frontend/src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart, faCartShopping, faUser, faSignOut, faBars } from '@fortawesome/free-solid-svg-icons';
+import { faHeart, faCartShopping, faUser, faSignOut, faBars, faGlobe } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 import SearchForm from '../SearchForm/SearchForm'
 import AuthContext from '../../contexts/AuthContext';
@@ -9,9 +9,15 @@ import './Header.css';
 function Header() {
     const { user } = useContext(AuthContext);
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [isPeripheryMenuOpen, setIsPeripheryMenuOpen] = useState(false);
 
     const toggleMenu = () => {
         setIsMenuOpen(prev => !prev);
+        setIsPeripheryMenuOpen(false);
+    };
+
+    const togglePeripheryMenu = () => {
+        setIsPeripheryMenuOpen(prev => !prev);
     };
 
 
@@ -34,9 +40,16 @@ function Header() {
                                 {/* Your dropdown menu content here */}
                                 <Link to="/catalog/computers" onClick={toggleMenu}>Computers</Link>
                                 <Link to="/catalog/monitors" onClick={toggleMenu}>Monitors</Link>
-                                <Link to="#">Periphery</Link>
+                                <Link to="#" onClick={togglePeripheryMenu}>Periphery</Link>
                                 <Link to="#">Accessories</Link>
                                 {/*<Link to="/catalog/others">Others</Link>*/}
+                            </div>
+                        )}
+                        {isPeripheryMenuOpen && (
+                            <div className="category-periphery-menu-content">
+                                <Link to="/catalog/keyboards" onClick={toggleMenu}>Keyboards</Link>
+                                <Link to="/catalog/mice" onClick={toggleMenu}>Mice</Link>
+
                             </div>
                         )}
                     </div>
@@ -58,6 +71,8 @@ function Header() {
                                     <FontAwesomeIcon icon={faHeart} />
                                 </Link>
 
+                                <Link to={'#'}><FontAwesomeIcon icon={faGlobe} /></Link>
+
                             </>
                         ) : (
                             <>
@@ -74,6 +89,7 @@ function Header() {
                                 <Link to="/likes">
                                     <FontAwesomeIcon icon={faHeart} />
                                 </Link>
+                                <Link to={'#'}><FontAwesomeIcon icon={faGlobe} /></Link>
 
                             </>
                         )}
