737c56e7741b7dd3004f259994980d788ed9b3b5
ValentinKardzhaliev <vkardjaliev@abv.bg>
2024-04-07 21:13:25 +0300

    Fixed width of the cart page design

--- pc_shop_frontend/src/components/Cart/Cart.jsx
+++ pc_shop_frontend/src/components/Cart/Cart.jsx
@@ -85,70 +85,72 @@ function Cart() {
 
     return (
         <div className="cart-container">
-            <div className="item-container">
-                <h2 className="cart-title">Your Cart</h2>
-                {cartItems.length === 0 ? (
-                    <p className="empty-cart-message">Your cart is empty.</p>
-                ) : (
-                    <div className="cart-items-list">
-                        {cartItems.map((item) => (
-                            <div className="product-card" key={item.product._id}>
-                                <img src={`${item.product.images[0].image}`} alt={item.product.name} className="cart-product-image" />
-                                <div className="cart-item-details">
-                                    <h2 className="cart-product-name">Name - {item.product.name}</h2>
-                                    <Link to={`/products/${item.product.type}/${item.product._id}`} className="cart-productDetails-link">View Full Characteristics</Link>
+            <h2 className="cart-title">Your Cart</h2>
+            <div className="item-container-buy-wrapper">
+                <div className="item-container">
+                    {cartItems.length === 0 ? (
+                        <p className="empty-cart-message">Your cart is empty.</p>
+                    ) : (
+                        <div className="cart-items-list">
+                            {cartItems.map((item) => (
+                                <div className="product-card" key={item.product._id}>
+                                    <img src={`${item.product.images[0].image}`} alt={item.product.name} className="cart-product-image" />
+                                    <div className="cart-item-details">
+                                        <h2 className="cart-product-name">Name - {item.product.name}</h2>
+                                        <Link to={`/products/${item.product.type}/${item.product._id}`} className="cart-productDetails-link">View Full Characteristics</Link>
+                                    </div>
+                                    <div className="cart-buttons-container">
+                                        <button className="cart-delete-button" onClick={() => removeProduct(item.product._id)}><FontAwesomeIcon icon={faTrash} /></button>
+                                        <div className="quantity-manipulation-container">
+                                            <div className="quantity-button decrease" onClick={() => decreaseQuantity(item.product._id)}>
+                                                <FontAwesomeIcon icon={faMinus} />
+                                            </div>
+                                            <span className="cart-product-quantity">{item.quantity}</span>
+                                            <div className="quantity-button increase" onClick={() => increaseQuantity(item.product._id)}>
+                                                <FontAwesomeIcon icon={faPlus} />
+                                            </div>
+                                            <div className="cart-product-price">${item.product.price * item.quantity}</div>
+
+                                        </div>
+                                    </div>
+
                                 </div>
-                                <div className="cart-buttons-container">
-                                    <button className="cart-delete-button" onClick={() => removeProduct(item.product._id)}><FontAwesomeIcon icon={faTrash} /></button>
-                                    <div className="quantity-manipulation-container">
-                                        <div className="quantity-button decrease" onClick={() => decreaseQuantity(item.product._id)}>
-                                            <FontAwesomeIcon icon={faMinus} />
-                                        </div>
-                                        <span className="cart-product-quantity">{item.quantity}</span>
-                                        <div className="quantity-button increase" onClick={() => increaseQuantity(item.product._id)}>
-                                            <FontAwesomeIcon icon={faPlus} />
-                                        </div>
-                                        <div className="cart-product-price">${item.product.price * item.quantity}</div>
+                            ))}
+                        </div>
+                    )}
+                </div>
+                <div className="buy-section">
+                    <div className='price-calculation-section'>
+                        <div className="calculation-all-products">
+                            {cartItems.map((item) => (
+                                <div key={item.product.name} className='calculation-item-container'>
+                                    <div className='calculation-item'>
+                                        <img src={`${item.product.images[0].image}`} alt={item.product.name} className='calculation-item-image' />
+                                        <span className='calculation-item-price'>{item.product.price * item.quantity}$</span>
 
                                     </div>
-                                </div>
-
-                            </div>
-                        ))}
-                    </div>
-                )}
-            </div>
-            <div className="buy-section">
-                <div className='price-calculation-section'>
-                    <div className="calculation-all-products">
-                        {cartItems.map((item) => (
-                            <div key={item.product.name} className='calculation-item-container'>
-                                <div className='calculation-item'>
-                                    <img src={`${item.product.images[0].image}`} alt={item.product.name} className='calculation-item-image' />
-                                    <span className='calculation-item-price'>{item.product.price * item.quantity}$</span>
+                                    <FontAwesomeIcon icon={faPlus} className='and-icon' />
 
                                 </div>
-                                <FontAwesomeIcon icon={faPlus} className='and-icon' />
+                            ))}
 
-                            </div>
-                        ))}
-
+                        </div>
+                        <div className="shipping-fee-container">
+                            <h3 className='shipping-fee-label'>Shipping fee:</h3>
+                            <span className="shipping-fee">7$</span>
+                        </div>
+                        <div className="divider-container">
+                            <hr className='total-price-divider' />
+                        </div>
+                        <div className="total-price-container">
+                            <h1 className='total-price-label'>Total price:</h1>
+                            <div className='total-price'>{Number(totalPrice + 7)}$</div>
+                        </div>
+                        <button className='purchase-button'>Buy</button>
                     </div>
-                    <div className="shipping-fee-container">
-                        <h3 className='shipping-fee-label'>Shipping fee:</h3>
-                        <span className="shipping-fee">7$</span>
-                    </div>
-                    <div className="divider-container">
-                        <hr className='total-price-divider' />
-                    </div>
-                    <div className="total-price-container">
-                        <h1 className='total-price-label'>Total price:</h1>
-                        <div className='total-price'>{Number(totalPrice + 7)}$</div>
-                    </div>
-                    <button className='purchase-button'>Buy</button>
                 </div>
-            </div>
 
+            </div>
         </div>
     );
 }
