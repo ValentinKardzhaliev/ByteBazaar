9facf0bbd4d10f7d39ecf1bd676641ea73fe919f
ValentinKardzhaliev <vkardjaliev@abv.bg>
2024-04-12 15:36:27 +0300

    Added allowed hosts

--- PC_shop_backend/PC_shop_backend/settings.py
+++ PC_shop_backend/PC_shop_backend/settings.py
@@ -17,9 +17,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!h93gq@_^p9se+^k1=tfusk747ixc&)%1@nxi%j$1!ip98-*=$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bytebazaar.pythonanywhere.com.pythonanywhere.com', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
