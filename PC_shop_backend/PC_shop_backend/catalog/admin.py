054a74ca5af4cd72b8a64aa0154ab8cf7407f2c6
ValentinKardzhaliev <vkardjaliev@abv.bg>
2024-05-21 19:15:36 +0300

    Fixed double laptop model admin registration

--- PC_shop_backend/PC_shop_backend/catalog/admin.py
+++ PC_shop_backend/PC_shop_backend/catalog/admin.py
@@ -34,18 +34,6 @@ class LaptopAdmin(admin.ModelAdmin):
         return super().formfield_for_choice_field(db_field, request, **kwargs)
 
 
-@admin.register(Laptop)
-class LaptopAdmin(admin.ModelAdmin):
-    list_display = ['name', 'type', 'processor', 'graphics', 'memory', 'storage']
-    inlines = [ProductImageInline]
-
-    def formfield_for_choice_field(self, db_field, request, **kwargs):
-        if db_field.name == 'type':
-            # Limit choices based on the model's type
-            return db_field.formfield(choices=[('computer', 'Computer')])
-        return super().formfield_for_choice_field(db_field, request, **kwargs)
-
-
 @admin.register(Monitor)
 class MonitorAdmin(admin.ModelAdmin):
     list_display = ['name', 'type', 'resolution', 'refresh_rate', 'panel_type', 'size']
