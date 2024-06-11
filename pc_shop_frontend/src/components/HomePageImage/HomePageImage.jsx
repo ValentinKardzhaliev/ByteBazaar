7fc8f0f4e9d5f5ef599d4373c803170bf90c5b2c
ValentinKardzhaliev <vkardjaliev@abv.bg>
2024-06-11 19:55:57 +0300

    Added a ad slider

--- pc_shop_frontend/src/components/HomePageImage/HomePageImage.jsx
+++ pc_shop_frontend/src/components/HomePageImage/HomePageImage.jsx
@@ -1,14 +1,37 @@
-import backgroundImage from '../../assets/images/asustufpromo-ecoms.webp'
-import './HomePageImage.css'
+import React, { useState } from 'react';
+import './HomePageImage.css';
+
+import image1 from '../../assets/images/asustufpromo-ecoms.webp';
+import image2 from '../../assets/images/msi-promo-ecom-main.webp';
+
+const images = [image1, image2];  // Add all images to this array
 
 function HomePageImage() {
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+    const handlePreviousClick = () => {
+        setCurrentImageIndex((prevIndex) => 
+            prevIndex === 0 ? images.length - 1 : prevIndex - 1
+        );
+    };
+
+    const handleNextClick = () => {
+        setCurrentImageIndex((prevIndex) => 
+            prevIndex === images.length - 1 ? 0 : prevIndex + 1
+        );
+    };
+
     return (
         <div className="first-image-container">
-            <img src={backgroundImage} alt="first-image" className='home-page-first-image'/>
+            <button onClick={handlePreviousClick} className="arrow left-arrow">&lt;</button>
+            <img 
+                src={images[currentImageIndex]} 
+                alt="home-page" 
+                className='home-page-first-image'
+            />
+            <button onClick={handleNextClick} className="arrow right-arrow">&gt;</button>
         </div>
-
-    )
+    );
 }
-
 
 export default HomePageImage;
