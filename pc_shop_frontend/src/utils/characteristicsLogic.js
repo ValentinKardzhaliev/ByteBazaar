9d3300fb0f1c1f98ada3d623b18592722681f16e
ValentinKardzhaliev <vkardjaliev@abv.bg>
2024-07-17 18:01:28 +0300

    Handled displaying laptops in the frontend

--- pc_shop_frontend/src/utils/characteristicsLogic.js
+++ pc_shop_frontend/src/utils/characteristicsLogic.js
@@ -2,41 +2,47 @@ function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-export function characteristicsLogic(props) {
-    let characteristics = {
-        computer: [],
-        monitor: [],
-        keyboard: [],
-    };
-    let typeOfProduct = props.product.type;
+const getComputerCharacteristics = (product) => {
+    const { processor, graphics, memory, storage } = product;
+    return [processor, graphics, memory, storage];
+};
 
-    switch (typeOfProduct) {
-        case 'computer':
-            characteristics[typeOfProduct].push(
-                props.product.processor,
-                props.product.graphics,
-                props.product.memory,
-                props.product.storage
-            );
-            break;
-        case 'monitor':
-            characteristics[typeOfProduct].push(
-                props.product.resolution,
-                props.product.refresh_rate,
-                props.product.size,
-                props.product.panel_type
-            );
-            break;
-        case 'keyboard':
-            characteristics[typeOfProduct].push(
-                capitalizeFirstLetter(props.product.key_switch_type),
-                props.product.backlight ? 'Backlit' : 'Not Backlit',
-                props.product.color,
-                props.product.wireless ? 'Wireless' : 'Wired'
-            );
-            break;
-        default:
-            break;
+const getLaptopCharacteristics = (product) => {
+    const { processor, graphics, memory, storage } = product;
+    return [processor, graphics, memory, storage];
+};
+
+const getMonitorCharacteristics = (product) => {
+    const { resolution, refresh_rate, size, panel_type } = product;
+    return [resolution, refresh_rate, size, panel_type];
+};
+
+const getKeyboardCharacteristics = (product) => {
+    const { key_switch_type, backlight, color, wireless } = product;
+    return [
+        capitalizeFirstLetter(key_switch_type),
+        backlight ? 'Backlit' : 'Not Backlit',
+        color,
+        wireless ? 'Wireless' : 'Wired'
+    ];
+};
+
+export function characteristicsLogic({ product }) {
+    const characteristicsMap = {
+        computer: getComputerCharacteristics,
+        monitor: getMonitorCharacteristics,
+        keyboard: getKeyboardCharacteristics,
+        laptop: getLaptopCharacteristics,
+    };
+
+    const { type } = product;
+    const getCharacteristics = characteristicsMap[type];
+
+    if (getCharacteristics) {
+        return {
+            [type]: getCharacteristics(product)
+        };
     }
-    return characteristics;
+
+    return {};
 }
