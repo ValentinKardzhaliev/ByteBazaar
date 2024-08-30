dc91752edc704d2c8e05292e0ca992cbf34eefeb
Bogomil-Simitchiev <simitchiev365@abv.bg>
2024-08-30 20:19:01 +0300

    Updated not found page

--- pc_shop_frontend/src/components/NotFoundPage/NotFoundPage.jsx
+++ pc_shop_frontend/src/components/NotFoundPage/NotFoundPage.jsx
@@ -1,10 +1,16 @@
-function NotFoundPage() {
+import React from 'react';
+import './NotFoundPage.css'; // Import the CSS file for styling
+import { Link } from 'react-router-dom';
 
-    return (
-        <h1>
-            Not Found Page!
-        </h1>
-    )
-}
+import notFoundImage from '../../assets/images/notFound.webp'; // Replace with your image path
+
+const NotFoundPage = () => {
+  return (
+    <div className="not-found-container">
+      <img src={notFoundImage} alt="Not Found" className="not-found-image" />
+      <p className="not-found-message">Sorry, the page you are looking for does not exist. <Link to={'/'}>Go to home page</Link></p>
+    </div>
+  );
+};
 
 export default NotFoundPage;
