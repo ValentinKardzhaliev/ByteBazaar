84788860821ccd8e4969848a6b02acdd872d63b1
Bogomil-Simitchiev <simitchiev365@abv.bg>
2024-08-30 17:49:06 +0300

    Changed styles in home page

--- pc_shop_frontend/src/components/HomePageUnderImage/HomePageUnderImage.jsx
+++ pc_shop_frontend/src/components/HomePageUnderImage/HomePageUnderImage.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import './HomePageUnderImage.css';
 
 import image2 from '../../assets/images/underImage.jpg';
 
 function HomePageUnderImage() {
     return (
-        <div className="first-image-container">
+        <div className="under-image-container">
             <img
                 src={image2}
                 alt="home-page"
-                className='home-page-first-image'
+                className='home-page-under-image'
             />
         </div>
     );
